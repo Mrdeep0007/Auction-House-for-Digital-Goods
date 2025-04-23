97c05d1543e4149a5565f02a45aa93c5ae7efcc7
Mrdeep0007 <deepakdhakadd2005@gmail.com>
2025-04-23 15:58:34 +0530

    Update AuctionHouseforDigitalGoods.sol

--- Contracts/AuctionHouseforDigitalGoods.sol
+++ Contracts/AuctionHouseforDigitalGoods.sol
@@ -1,1 +1,34 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AuctionHouse {
+    address public owner;
+    address public highestBidder;
+    uint256 public highestBid;
+    bool public auctionEnded;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function placeBid() public payable {
+        require(!auctionEnded, "Auction already ended");
+        require(msg.value > highestBid, "Bid not high enough");
+
+        if (highestBid != 0) {
+            payable(highestBidder).transfer(highestBid); // Refund previous highest bidder
+        }
+
+        highestBidder = msg.sender;
+        highestBid = msg.value;
+    }
+
+    function endAuction() public {
+        require(msg.sender == owner, "Only owner can end the auction");
+        require(!auctionEnded, "Auction already ended");
+
+        auctionEnded = true;
+        payable(owner).transfer(highestBid);
+    }
+}
